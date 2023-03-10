d22fdb8df2e3fd841d4639d46ae40ae42c4b797a
nithin0111 <programming965@gmail.com>
2023-03-10 21:07:05 +0530

    different ways to write hello world

--- hello-world.rb
+++ hello-world.rb
@@ -1,3 +1,17 @@
 puts "Hello world"
-print "Hello world"
+# this doesnot have a new line at the end to add
+print "Hello world\n"
 p "Hello world"
+
+
+greeting = "Hello ruby!!"
+
+puts greeting
+
+# function to say hello
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+# calling the function and passing the argument
+say_hello "Hello ruby world!!"
